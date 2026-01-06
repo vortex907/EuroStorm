a6bf3fe67ca2041f0be7fe84977a1bd3f1e9c9c3
kalinx <ahmet83973@gmail.com>
2026-01-06 22:55:20 +0100

    fixes

---
+++ common/defines/realnuke_defines.lua
@@ -0,0 +1,9 @@
+NDefines.NCountry.NUCLEAR_PRODUCTION_SCALE = 3650							-- +1 nuclear_production gives 0.1 nuke per year (vanilla 365 -> 3550)
+NDefines.NCountry.NUCLEAR_BOMB_DROP_WAR_SUPPORT_EFFECT_MAX_INFRA = 2.0 		-- Reduce enemy national war support on nuking a province, the value scales with infrastructure up to this number (vanilla 0.2 -> 2.0)
+NDefines.NCountry.NUCLEAR_BOMB_DROP_WAR_SUPPORT_EFFECT_MAX_VP = 5			-- War support will be scaled down if there's less VP than this in the province (vanilla 3 -> 5)
+
+NDefines.NMilitary.NUKE_MIN_DAMAGE_PERCENT = 1.50							-- Minimum damage from nukes as a percentage of current strength/organisation (vanilla 0.1 -> 1.50)
+NDefines.NMilitary.NUKE_MAX_DAMAGE_PERCENT = 2.00							-- Maximum damage from nukes as a percentage of current strength/organisation (vanilla 0.9 -> 2.00)
+NDefines.NMilitary.NUKE_DELAY_HOURS = 2											-- How many hours does it take for the nuclear drop to happen (vanilla 12 -> 2)
+
+NDefines.NAir.STRATEGIC_BOMBER_NUKE_AIR_SUPERIORITY = 0.90					-- How much air superiority is needed for a tactical bomber to be able to nuke a province (vanilla 0.75 -> 0.9)
